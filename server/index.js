e6a4cabf3a2512c530449c2e95a7824cc7496fd8
devumairazmat <umairazmat8260@gmail.com>
2022-08-23 04:29:12 +0500

    express nodemon body-parser

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./db/index");
+
+const app = express();
+
+const productRouter =  require("./routes/productRouter");
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+db.on("error", console.error.bind(console, "MongoDB Connection error"));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Food Ordering App!" });
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+app.use('/api/',productRouter);
