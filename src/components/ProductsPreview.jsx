f7b19df8895967cf2e344d4f11b842c36479640e
devumairazmat <umairazmat8260@gmail.com>
2022-08-25 03:04:59 +0500

    product crousel done

--- src/components/ProductsPreview.jsx
+++ src/components/ProductsPreview.jsx
@@ -1,25 +1,51 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { ProductPreviewCard } from "../components/ProductPreviewCard";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 function ProductsPreview() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   useEffect(() => {
-    fetch('http://localhost:8080/api/products')
-        .then(response => response.json())
-        .then(data => setProducts(data?.data))
-        .catch(e => console.log(e))
-}, [])
+    fetch("http://localhost:8080/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data?.data))
+      .catch((e) => console.log(e));
+  }, []);
   return (
     <>
-      <div className="container mx-auto pb-4 w-2/3 text-white bg-black">
-        <h2>Products</h2>
-        {products.length > 0 &&
-          products.map((product, index) => {
-            return <ProductPreviewCard key={index} product={product} />;
-          })}
+      <div className="w-full text-white bg-black">
+        <Carousel responsive={responsive}>
+          {products.length > 0 &&
+            products.map((product, index) => {
+              return (
+                <div className="w-full p-4">
+                  <ProductPreviewCard key={index} product={product} />
+                </div>
+              );
+            })}
+        </Carousel>
       </div>
     </>
   );
