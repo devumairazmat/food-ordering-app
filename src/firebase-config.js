f1fda64cf809e8623e97eafcdcd1f0148a0e881d
devumairazmat <umairazmat8260@gmail.com>
2022-08-27 03:20:41 +0500

    firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7VyR9cBhRYFazHdkMb680-V58YvoC-gs",
+  authDomain: "food-delivry-app.firebaseapp.com",
+  projectId: "food-delivry-app",
+  storageBucket: "food-delivry-app.appspot.com",
+  messagingSenderId: "712796676890",
+  appId: "1:712796676890:web:5d3760d0a708cab12f02a3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/Login/Index.jsx
+++ src/pages/Login/Index.jsx
@@ -51,7 +51,7 @@ const Login = () => {
             <div className="rounded-lg max-w-md w-full flex flex-col items-center justify-center relative">
                 <div className="absolute inset-0 transition duration-300 animate-pink blur  gradient bg-gradient-to-tr from-rose-500 to-yellow-500"></div>
                 <div className="p-10 rounded-xl z-10 w-full h-full bg-black">
-                    <h5 className="text-3xl">Login</h5>
+                    <h5 className="text-3xl">Login From</h5>
                 <form className="w-full space-y-6" onSubmit={handleSubmit(onSubmit)}>
                     <div>
                         <label 
